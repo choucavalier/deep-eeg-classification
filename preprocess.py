6ef59848f2fd454e8978058ca12c31393a409f7e
Valentin Iovene <tgy@inria.fr>
2019-05-08 15:23:05 +0200

    update

--- preprocess.py
+++ preprocess.py
@@ -17,8 +17,8 @@ from joblib import Parallel, delayed
 from mne_features.feature_extraction import extract_features, FeatureExtractor
 
 SELECTED_CH_NAMES = np.array([
-    'A1', 'A2', 'C3', 'C4', 'CZ', 'F3', 'F4', 'F7', 'F8', 'FP1', 'FP2', 'FZ',
-    'O1', 'O2', 'P3', 'P4', 'PZ', 'T3', 'T4', 'T5', 'T6'
+  'A1', 'A2', 'C3', 'C4', 'CZ', 'F3', 'F4', 'F7', 'F8', 'FP1', 'FP2', 'FZ',
+  'O1', 'O2', 'P3', 'P4', 'PZ', 'T3', 'T4', 'T5', 'T6'
 ])
 
 SFREQ = 100
@@ -27,53 +27,56 @@ SFREQ = 100
 def preprocess_one_file(path):
   f = mne.io.read_raw_edf(path)
   cleaned_ch_names = np.array([
-      c.replace('EEG ', '').replace('-REF', '') for c in f.ch_names
+    c.replace('EEG ', '').replace('-REF', '') for c in f.ch_names
   ])
   ch_idxs = np.array([
-      np.where(cleaned_ch_names == ch)[0][0] for ch in SELECTED_CH_NAMES
+    np.where(cleaned_ch_names == ch)[0][0] for ch in SELECTED_CH_NAMES
   ])
   f = f.load_data().copy().resample(SFREQ, npad='auto')
   x = f.get_data()[ch_idxs, :]
-  rnd_start_idx = np.random.randint(
-      int(60 * SFREQ), int(x.shape[1] - (60 * SFREQ))
+  rnd_start_idxs = np.random.randint(
+    int(60 * SFREQ),
+    int(x.shape[1] - (60 * SFREQ)),
+    size=max(1, x.shape[1] // (60 * SFREQ) // 5)
   )
-  x = x[:, rnd_start_idx:int(rnd_start_idx + 60 * SFREQ)]
+  x = np.vstack([
+    x[np.newaxis, :, idx:int(idx + 60 * SFREQ)] for idx in rnd_start_idxs
+  ])
   x = x.clip(-800, 800)
-  y = np.array([['abnormal' in path]])
+  y = np.repeat(int('abnormal' in path), x.shape[0])
   return (path, x, y)
 
 
 def preprocess_dataset(output_dir):
   data_paths = glob.glob(
-      '/storage/inria/viovene/tuh_data/**/*.edf', recursive=True
+    '/storage/inria/viovene/tuh_data/**/*.edf', recursive=True
   )
   np.random.shuffle(data_paths)
-  # data = [preprocess_one_file(path) for path in data_paths]
   data = Parallel(n_jobs=30
                   )(delayed(preprocess_one_file)(path) for path in data_paths)
   train, test = train_test_split(data)
-  x_train = np.vstack([x[np.newaxis, :, :] for (_, x, _) in train])
-  x_test = np.vstack([x[np.newaxis, :, :] for (_, x, _) in test])
-  y_train = np.vstack([y for (_, _, y) in train])
-  y_test = np.vstack([y for (_, _, y) in test])
+  x_train = np.vstack([x for (_, x, _) in train])
+  x_test = np.vstack([x for (_, x, _) in test])
+  y_train = np.concatenate([y for (_, _, y) in train])
+  y_test = np.concatenate([y for (_, _, y) in test])
   np.save(os.path.join(output_dir, 'x_train_raw.npy'), x_train)
   np.save(os.path.join(output_dir, 'x_test_raw.npy'), x_test)
   funcs = {
-      'skewness',
-      'kurtosis',
-      'mean',
-      'variance',
-      'std',
-      'ptp_amp',
-      'hurst_exp',
-      'app_entropy',
-      'pow_freq_bands',
-      'hjorth_complexity',
+    'skewness',
+    'kurtosis',
+    'mean',
+    'variance',
+    'std',
+    'ptp_amp',
+    'hurst_exp',
+    'app_entropy',
+    'pow_freq_bands',
+    'hjorth_complexity',
   }
   params = {
-      'pow_freq_bands__freq_bands': np.array([0.5, 4, 8, 13, 30, 49]),
-      'pow_freq_bands__ratios': 'all',
-      'pow_freq_bands__log': True
+    'pow_freq_bands__freq_bands': np.array([0.5, 4, 8, 13, 30, 49]),
+    'pow_freq_bands__ratios': 'all',
+    'pow_freq_bands__log': True
   }
   fe = FeatureExtractor(sfreq=SFREQ, selected_funcs=funcs, params=params)
   x_train = fe.fit_transform(x_train)
--- train_network.py
+++ train_network.py
@@ -10,44 +10,8 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.base import TransformerMixin
 from sklearn.model_selection import train_test_split
 
-
-class EEGCustomScaler(TransformerMixin):
-  def __init__(self, **kwargs):
-    self._scaler = StandardScaler(copy=True, **kwargs)
-    self._orig_shape = None
-
-  def fit(self, X, **kwargs):
-    X = np.array(X)
-    # Save the original shape to reshape the flattened X later
-    # back to its original shape
-    if len(X.shape) > 1:
-      self._orig_shape = X.shape[1:]
-    X = self._flatten(X)
-    self._scaler.fit(X, **kwargs)
-    return self
-
-  def transform(self, X, **kwargs):
-    X = np.array(X)
-    X = self._flatten(X)
-    X = self._scaler.transform(X, **kwargs)
-    X = self._reshape(X)
-    return X
-
-  def _flatten(self, X):
-    # Reshape X to <= 2 dimensions
-    if len(X.shape) > 2:
-      n_dims = np.prod(self._orig_shape)
-      X = X.reshape(-1, n_dims)
-    return X
-
-  def _reshape(self, X):
-    # Reshape X back to it's original shape
-    if len(X.shape) >= 2:
-      X = X.reshape(-1, *self._orig_shape)
-    return X
-
-
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
+torch.cuda.set_device(1)
 
 data_dir = '/storage/inria/viovene/preprocessed_data'
 mkpath = lambda path: os.path.join(data_dir, path)
@@ -56,16 +20,12 @@ x_test = np.load(mkpath('x_test_raw.npy')).astype(np.float32)
 y_train = np.load(mkpath('y_train.npy')).astype(np.int64).flatten()
 y_test = np.load(mkpath('y_test.npy')).astype(np.int64).flatten()
 x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train)
-x_train = x_train[:, :, :368] * 1e6
-x_valid = x_valid[:, :, :368] * 1e6
-x_test = x_test[:, :, :368] * 1e6
-# x_scaler = EEGCustomScaler().fit(x_train)
-# x_train = x_scaler.transform(x_train)
-# x_valid = x_scaler.transform(x_valid)
-# x_test = x_scaler.transform(x_test)
+x_train = x_train * 1e6
+x_valid = x_valid * 1e6
+x_test = x_test * 1e6
 x_train = x_train[:, :, :, np.newaxis]
-x_valid = x_valid[:, :, :, np.newaxis]
-x_test = x_test[:, :, :, np.newaxis]
+x_valid = x_valid[:, :, :368, np.newaxis]
+x_test = x_test[:, :, :368, np.newaxis]
 
 
 class Expression(torch.nn.Module):
@@ -88,21 +48,24 @@ model = torch.nn.Sequential(
   torch.nn.Softmax(dim=1),
   Expression(lambda x: x[:, :, 0, 0]),
 )
-
 model.cuda()
 
-learning_rate = 1e-3
+learning_rate = 3e-4
 loss_fn = torch.nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 
-def get_batch():
-  batch_size = 64
+def get_batch(batch_size=512, seq_len=368):
   idxs = np.random.randint(0, x_train.shape[0], size=batch_size)
-  return (
-    torch.from_numpy(x_train[idxs]).cuda(),
-    torch.from_numpy(y_train[idxs]).cuda(),
-  )
+  sidxs = np.random.randint(0, x_train.shape[2] - seq_len, size=batch_size)
+  x_batch = torch.from_numpy(
+    np.concatenate([
+      x_train[idxs[i], :, sidxs[i]:(sidxs[i] + seq_len)][np.newaxis]
+      for i in range(batch_size)
+    ],
+                   axis=0)
+  ).cuda()
+  return (x_batch, torch.from_numpy(y_train[idxs]).cuda())
 
 
 def get_model_accuracy(x, y):
@@ -120,10 +83,10 @@ while True:
     loss.backward()
     optimizer.step()
   model.eval()
-  train_accuracy = get_model_accuracy(x_train, y_train)
+  train_accuracy = get_model_accuracy(x_train[:, :, :368, :], y_train)
   valid_accuracy = get_model_accuracy(x_valid, y_valid)
-  # test_accuracy = get_model_accuracy(x_test, y_test)
+  test_accuracy = get_model_accuracy(x_test, y_test)
   print('accuracy report')
   print(f'\ttrain\t{train_accuracy}')
   print(f'\tvalid\t{valid_accuracy}')
-  # print(f'\ttest\t{test_accuracy}')
+  print(f'\ttest\t{test_accuracy}')
